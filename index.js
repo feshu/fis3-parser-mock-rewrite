a237c1fc74980cecc68cea1f291a96b15e915bf9
张舒 <zhangshu@stnts.com>
2016-07-04 14:36:08 +0800

    add eExt & rename config

--- index.js
+++ index.js
@@ -11,13 +11,24 @@ module.exports = function (content, file, settings) {
     var rPath = settings.rPath || 'mock';
     var include = settings.include || '*.json';
     var exclude = settings.exclude || null;
+    var rExt = (settings.rExt || '').replace(/\./, '\\.');
+    var rename = settings.rename || null;
     var files = fis.util.find(fis.project.getProjectPath(rPath), include, exclude);
     var result = [];
 
     for (var i = 0; i < files.length; i++) {
         var path = fis.util.ext(files[i]);
-        result.push('rewrite ^\/' + path.filename.replace(/_/, '\\/') + '$ /' + rPath + '/' + path.basename);
+        var arr = [];
+        arr.push('rewrite');
+        if (typeof rename == 'function') {
+            arr.push(rename(path, settings));
+        } else {
+            arr.push('^\/' + path.filename.replace(/_/, '\\/') + rExt + '$');
+        }
+        arr.push('/' + rPath + '/' + path.basename);
+        result.push(arr.join(' '));
     }
+
     content = content.replace(/__REWRITE_RULES__/, result.join('\n'));
     return content;
 };
